e192243a553a4b73e4f8893b8ccc7a00d2533d6e
john <john@chinawebeye.com>
2018-11-15 21:10:35 +0800

    add 3

--- test.py
+++ test.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import random
 import datetime
 
-if __name__ == '__main__':
+
+def test1():
+    df = pd.DataFrame(((random.randint(2012, 2016), random.choice(['tech', 'art', 'office']),
+                        '%dk-%dk' % (random.randint(2, 10), random.randint(10, 20)), 'tt') for _ in range(10000)),
+                      columns=['publish_time', 'classf', 'salary', 'title'])
+    df.head()
+    dd = df.groupby(['publish_time', 'classf', 'salary']).count()['title'].groupby(level=0, group_keys=False).nlargest(10)
+    print(dd)
+
+
+def test2():
     m_cache_expire_time = 15
     now = datetime.datetime.now()
-
     interval = (now.minute * 60 + now.second) % m_cache_expire_time
     print(interval, m_cache_expire_time - interval)
+
+
+def main():
+    test1()
+
+if __name__ == '__main__':
+    main()
+
 
